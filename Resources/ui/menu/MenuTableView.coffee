e8437de1f142539e92bdc1e0828d69c6b4b135b7
Yuchen Zhang <yuchenzhang@me.com>
2012-03-28 11:32:14 +0200

    WIP

--- Resources/ui/menu/MenuTableView.coffee
+++ Resources/ui/menu/MenuTableView.coffee
@@ -7,8 +7,7 @@ class MenuTableView
       row_view = Ti.UI.createView({
         width:280,
         height: 60,
-        left:0,
-        borderColor: 'black'
+        left:0
       })  
       picture =  Titanium.UI.createImageView({
         image:dish.picture,
@@ -32,23 +31,63 @@ class MenuTableView
         width:20,
         height:20,
         top: 2,
-        right: 40
+        right: 25
       })
       
       price = Ti.UI.createLabel({
        text: dish.price,
-       font:{fontSize:12,fontWeight:'bold'},
+       font:{fontSize:12,fontWeight:'normal'},
        textAlign: 'right',
        width: 20,
        height:20,
        top: 2,
-       right: 18 
+       right: 2 
+      })
+      
+      thumb = Ti.UI.createImageView({
+        image:"/images/Titanium_Iconpack_1/light/light_thumb-up.png",
+        width:20,
+        height:20,
+        bottom: 3,
+        right: 50
+      })
+      
+      recommends = Ti.UI.createLabel({
+       text: dish.recommends,
+       font:{fontSize:11,fontWeight:'normal'},
+       textAlign: 'right',
+       width: 20,
+       height:20,
+       bottom: 2,
+       right: 40 
+      })
+      
+      bubble = Ti.UI.createImageView({
+        image:"/images/Titanium_Iconpack_1/light/light_comment.png",
+        width:20,
+        height:20,
+        bottom: 2,
+        right: 15
+      })
+      
+      reviews = Ti.UI.createLabel({
+       text: dish.recommends,
+       font:{fontSize:11,fontWeight:'normal'},
+       textAlign: 'right',
+       width: 20,
+       height:20,
+       bottom: 2,
+       right: 2 
       })
          
       row_view.add picture
       row_view.add name
       row_view.add currency
       row_view.add price
+      row_view.add thumb
+      row_view.add recommends
+      row_view.add bubble
+      row_view.add reviews
       dish_row.add row_view
       rows.push dish_row   
     
--- Resources/ui/menu/MenuTableView.js
+++ Resources/ui/menu/MenuTableView.js
@@ -4,7 +4,7 @@
   MenuTableView = (function() {
 
     function MenuTableView(dishes) {
-      var currency, dish, dish_row, name, num, picture, price, row_view, rows, tableview, _len;
+      var bubble, currency, dish, dish_row, name, num, picture, price, recommends, reviews, row_view, rows, tableview, thumb, _len;
       rows = [];
       for (num = 0, _len = dishes.length; num < _len; num++) {
         dish = dishes[num];
@@ -14,8 +14,7 @@
         row_view = Ti.UI.createView({
           width: 280,
           height: 60,
-          left: 0,
-          borderColor: 'black'
+          left: 0
         });
         picture = Titanium.UI.createImageView({
           image: dish.picture,
@@ -40,24 +39,66 @@
           width: 20,
           height: 20,
           top: 2,
-          right: 40
+          right: 25
         });
         price = Ti.UI.createLabel({
           text: dish.price,
           font: {
             fontSize: 12,
-            fontWeight: 'bold'
+            fontWeight: 'normal'
           },
           textAlign: 'right',
           width: 20,
           height: 20,
           top: 2,
-          right: 18
+          right: 2
+        });
+        thumb = Ti.UI.createImageView({
+          image: "/images/Titanium_Iconpack_1/light/light_thumb-up.png",
+          width: 20,
+          height: 20,
+          bottom: 3,
+          right: 50
+        });
+        recommends = Ti.UI.createLabel({
+          text: dish.recommends,
+          font: {
+            fontSize: 11,
+            fontWeight: 'normal'
+          },
+          textAlign: 'right',
+          width: 20,
+          height: 20,
+          bottom: 2,
+          right: 40
+        });
+        bubble = Ti.UI.createImageView({
+          image: "/images/Titanium_Iconpack_1/light/light_comment.png",
+          width: 20,
+          height: 20,
+          bottom: 2,
+          right: 15
+        });
+        reviews = Ti.UI.createLabel({
+          text: dish.recommends,
+          font: {
+            fontSize: 11,
+            fontWeight: 'normal'
+          },
+          textAlign: 'right',
+          width: 20,
+          height: 20,
+          bottom: 2,
+          right: 2
         });
         row_view.add(picture);
         row_view.add(name);
         row_view.add(currency);
         row_view.add(price);
+        row_view.add(thumb);
+        row_view.add(recommends);
+        row_view.add(bubble);
+        row_view.add(reviews);
         dish_row.add(row_view);
         rows.push(dish_row);
       }
